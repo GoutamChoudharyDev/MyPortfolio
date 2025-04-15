d9e736b910beeb8d7cc667d8a7b9fbecf859cca4
unknown <rc.goutam.choudhary@gmail.com>
2025-04-15 21:27:34 +0530

    scroll update

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -10,19 +10,19 @@ const Navbar = () => {
             <div className="nav-logo"><span className='color-text'>GOUTAM</span></div>
 
             <div className={`nav-menu ${menuOpen ? "open" : ""}`}>
-                <li><Link to="home" smooth={true} duration={700} className="anchor-link">
+                <li><Link to="home" smooth={true} duration={500} className="anchor-link">
                     Home
                 </Link></li>
-                <li><Link to="about" smooth={true} duration={700} offset={-50} className="anchor-link">
+                <li><Link to="about" smooth={true} duration={500} offset={-50} className="anchor-link">
                     About Me
                 </Link></li>
-                <li><Link to="work" smooth={true} duration={700} offset={-50} className="anchor-link">
+                <li><Link to="work" smooth={true} duration={500} offset={-50} className="anchor-link">
                     My Work
                 </Link></li>
-                <li><Link to="contact" smooth={true} duration={700} offset={-50} className="anchor-link">
+                <li><Link to="contact" smooth={true} duration={500} offset={-50} className="anchor-link">
                     Contact
                 </Link></li>
-                <li><Link to="footer" smooth={true} duration={700} offset={-50} className="anchor-link">
+                <li><Link to="footer" smooth={true} duration={500} offset={-50} className="anchor-link">
                     Portfolio
                 </Link></li>
             </div>
